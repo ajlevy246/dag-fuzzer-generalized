5718bc81d75fa03a165a0f45baf4f8d4cdf6832c
ajlevy246 <ajlevy246@gmail.com>
2026-03-02 16:12:00 -0500

    update which node attributes are preserved vs. discarded

--- src/main/scala/fuzzer/utils/generation/dag/DFGSerializer.scala
+++ src/main/scala/fuzzer/utils/generation/dag/DFGSerializer.scala
@@ -8,6 +8,7 @@ example.
 package fuzzer.utils.generation.dag
 
 import play.api.libs.json.{JsObject, JsValue, JsString, JsNumber, JsNull, JsArray, Json}
+import scala.util.matching.Regex
 import fuzzer.core.graph.{DFOperator, Graph, Node}
 
 object DFGSerializer {
@@ -16,14 +17,14 @@ object DFGSerializer {
 
     // Nodes capture id, operator name, and a table binding (for source nodes)
     val nodeArray = JsArray(
-        graph.nodesMap.values.toSeq.sortBy(_.id).map { node =>
+        graph.nodesMap.values.toSeq.sortBy(_.id.stripPrefix("node_").toInt).map { node =>
             val op = node.value
             val isSource = node.parents.isEmpty
 
             // Fields shared by all nodes, regardless of their role
             val baseFields = Seq(
                 "id"       -> JsString(node.id), // node id
-                "numId"    -> JsNumber(op.id), // numerical identifier for the specific DF operator
+                // "depth"    -> JsNumber(op.id), // operator id? node depth? not needed?
                 "operator" -> (if (op.name != null) JsString(op.name) else JsNull), // name of the specific oeprator. May be null. 
                 "role"     -> JsString( 
                     if (isSource)         "source" // Load data
@@ -33,11 +34,15 @@ object DFGSerializer {
             )
 
             // Only source nodes have a bound table (via `.state`)
+            val suffixPattern: Regex = """_node_\d+$""".r
             val tableField: Seq[(String, JsValue)] = 
                 if (isSource && op.state != null)
-                    Seq("table" -> JsString(op.state.originalIdentifier))
+                    // Seq("table" -> JsString(op.state.originalIdentifier))
+                    // Seq("table" -> JsString(op.state.identifier))
+                    // Strip node suffix? 
+                    Seq("table" -> JsString(suffixPattern.replaceFirstIn(op.state.originalIdentifier, "")))
                 else
-                    Seq.empty
+                    Seq.empty   
 
             JsObject(baseFields ++ tableField)
         }
@@ -45,7 +50,7 @@ object DFGSerializer {
 
     // Edges are derived from the children mappings, sorted by the source,
     val edgeArray = JsArray(
-        graph.children.toSeq.sortBy(_._1).flatMap { case (fromId, toIds) =>    
+        graph.children.toSeq.sortBy(_._1.stripPrefix("node_").toInt).flatMap { case (fromId, toIds) =>    
             toIds.map { toId => 
                 Json.obj("from" -> fromId, "to" -> toId)
             }
